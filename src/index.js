43c4195ff13e9a9178e46d0101cd340baf34e39f
Alex Nikitin <alexnikicin@gmail.com>
2022-03-07 19:46:39 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+  let str = Math.abs(Number(n));
+  str = String(str);
   
+  let i = str.length - 1;
+      let result = "";
+      
+      while (i >= 0) {
+        result = (result + str[i]);
+        i = i - 1;
+      }
+      return (result); 
 }
